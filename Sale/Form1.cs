43040f9160d4c171e276bee3cc88cf57f213aabb
Stekoks <Stekoks@DESKTOP-4SK39GD>
2023-11-10 10:25:25 +0200

    Add project files.

---
+++ Sale/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Sale
+{
+public partial class Form1 : Form
+    {
+        string connectionString = "Data Source=DESKTOP-4SK39GD;Initial Catalog=ITacademy;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Customers");
+            comboBox1.Items.Add("Sellers");
+            comboBox1.Items.Add("Sales");
+            comboBox1.SelectedIndex = 0;
+            DisplayData();
+        }
+        private void DisplayData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "select * from " + comboBox1.Text;
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                dataGridView1.DataSource = dt;
+            }
+        }
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
